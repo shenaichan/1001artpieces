6f167e1fb405d3a4668c40780eaba62244d8d1d3
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2024-12-09 19:31:19 -0800

    Set up Express + TypeScript server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
